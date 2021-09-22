d54990fed709aae6d35dbe9d9027ea3e5d0ffa2e
Srijan Samridh <74289654+SrijanSamridh@users.noreply.github.com>
2021-09-22 12:32:33 +0530

    Update BubbleSort.py

--- BubbleSort.py
+++ BubbleSort.py
@@ -5,7 +5,7 @@ nums = [5, 3, 8, 6, 7, 2]
 def BubbleSort(nums):
      for i in range(len(nums)-1, 0, -1):
          for j in range(i):
-             if nums[j] > nums[j]+1:
+             if nums[j] > nums[j+1]:
                  temp = nums[j]
                  nums[j] = nums[j+1]
                  nums[j+1] = temp
